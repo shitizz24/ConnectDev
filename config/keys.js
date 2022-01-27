2fc6f320b2db6de8163c958b7d552c2ef2f775df
Shitiz Khanna <shitizkhanna2@gmail.com>
2022-01-27 13:45:48 +0530

    trigger commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodbURI:
-    "mongodb+srv://shitiz:1234@cluster0-xbbjb.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://shitiz:1234@cluster0-xbbjb.mongodb.net/test?retryWrites=true&w=majority", 
   secret: "connectdev"
 };
